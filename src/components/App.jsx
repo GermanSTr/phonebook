1df5faf87bb0604de928400958c9f28d04c632a3
GermanSTr <gershev1994@gmail.com>
2024-02-05 00:02:14 +0200

    route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
 import { ContactList } from './ContactList/ContactList';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
+import { Route, Routes } from 'react-router-dom';
 
 export const App = () => {
   return (
     <div>
+      <header>
+        <Routes>
+          <Route path='/register' element={ } />
+          <Route path='/login' element={ }/>
+          <Route path='/contacts' element={ }/>
+        </Routes>
+      </header>
       <h1>Phonebook</h1>
       <ContactForm />
 
